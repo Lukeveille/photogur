f067420f90eec4b8346f1dcc6fae1685da9eabfb
Lukeveille <luke@lukeleveille.com>
2018-10-04 14:21:46 -0400

    Setting up session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
-      session[:user_is=d] = user.id
+      session[:user_id] = user.id
       redirect_to pictures_url, notice: "Logged in!"
     else
       render :new
